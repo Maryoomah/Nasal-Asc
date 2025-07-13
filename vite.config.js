85e254c49002c1e1000f43067ae5c555aab9c9db
Maryoomah <148255944+Maryoomah@users.noreply.github.com>
2025-07-13 12:38:23 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Replace with your actual GitHub repo name
 export default defineConfig({
   base: "/Nasal-Asc/",
   plugins: [react()],
